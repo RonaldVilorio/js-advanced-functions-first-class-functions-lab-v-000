4b188efc57e076d070482f8339f0ff35bad8028b
ronald vilorio <soccer23415@gmail.com>
2018-05-15 20:05:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const returnFirstTwoDrivers = function (drivers){
   // debugger
   return drivers.slice(0,2).map(function(driver){return driver})
 }
+const returnLastTwoDrivers = function(drivers){
+  return drivers.slice(drivers.length - 1, -2)
+}
