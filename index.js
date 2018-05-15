41d64a919b8bcb7bd97926abba5b37d404bc4c4b
ronald vilorio <soccer23415@gmail.com>
2018-05-15 20:17:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ const createFareMultiplier = function(num){
   return function(num){return num * num}
 }
 
-const fareDoubler = function(){
-  
+const fareDoubler = function(fare){
+  return fare * 2
 }
