398a80fa5c2bfd8bfcb061d2ceb9c41bc6d8205a
ronald vilorio <soccer23415@gmail.com>
2018-05-15 20:15:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ const returnLastTwoDrivers = function(drivers){
 const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
 
 const createFareMultiplier = function(num){
-  debugger
+  // debugger
 }
