8061a192c2b6e4f84e74bf01e22fb837456ec77b
ronald vilorio <soccer23415@gmail.com>
2018-05-15 20:04:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-const first_two_drivers = function returnFirstTwoDrivers(drivers){
+const returnFirstTwoDrivers = function (drivers){
   // debugger
   return drivers.slice(0,2).map(function(driver){return driver})
 }
